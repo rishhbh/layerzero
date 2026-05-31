9c1f00ec9ecd50ed7993167d83f76de24de8b67f
rishabh.sharma@revlabz.ai <rkvmj72@gmail.com>
2026-05-31 17:02:01 +0530

    feature: implemented login route and generate token function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,20 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import { generateToken } from '../config/utils.js';
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         if (!name || !email || !password) {
-            res.status(400).json({
+            return res.status(400).json({
                 message: 'Please fill out all the required fields!'
             });
         }
 
         const userExists = await User.findOne({ email });
         if (userExists) {
-            res.status(400).json({
+            return res.status(400).json({
                 message: 'This user is already registerd with us!'
             });
         }
@@ -26,8 +27,52 @@ const registerUser = async (req, res) => {
             email,
             password: hashPassword
         });
-    } catch {
 
+        generateToken(newUser._id, res);
+        res.status(200).json({
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+        });
+    } catch {
+        res.status(400).json({
+            message: "Some error occured while registering the user!"
+        });
     }
 
+};
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please fill out the required fields!"
+            });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "The user doesn't exist!"
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({
+                message: "Invalid Credentials!"
+            });
+        }
+
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+        });
+    } catch {
+        res.status(500).json({
+            message: "Some error occurred while logging in the user!"
+        });
+    }
 };
