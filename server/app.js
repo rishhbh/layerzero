890b21b04216f3d3bca9a85219883418f4c544bd
rishabh.sharma@revlabz.ai <rkvmj72@gmail.com>
2026-06-01 23:31:55 +0530

    feat: added cookie parser

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ import express from 'express';
 import connectWithDB from './config/db.js';
 import aiChatRoute from './routes/aiChatRoute.js'
 import authRoute from './routes/authRoute.js'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/chat', aiChatRoute);
 app.use('/api/auth', authRoute);
+
 
 app.get('/', (req, res) => {
     res.json({
