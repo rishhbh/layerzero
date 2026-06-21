72b39af22fe1762c572161d2c4a9c9060e9dd6b7
rishabh.sharma@revlabz.ai <rkvmj72@gmail.com>
2026-06-21 21:53:18 +0530

    feat: add user timestamps, update environment variables, and implement health check endpoint

--- server/app.js
+++ server/app.js
@@ -1,39 +1,49 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import connectWithDB from './config/db.js';
-import authRoute from './routes/authRoute.js';
-import ingestRoute from './routes/ingestRoute.js';
-import handleError from './middlewares/errorHandler.js';
-import cookieParser from 'cookie-parser';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectWithDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import ingestRoute from "./routes/ingestRoute.js";
+import handleError from "./middlewares/errorHandler.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('trust proxy', 1);
-app.use(cors({
+app.set("trust proxy", 1);
+app.use(
+  cors({
     origin: [
-        process.env.NODE_ENV === 'development' 
-        ? ['http://localhost:5173'] 
+      process.env.NODE_ENV === "development"
+        ? ["http://localhost:5173"]
         : [process.env.CLIENT_URL],
     ],
     credentials: true,
-    exposedHeaders: ["set-cookie"]
-}));
+    exposedHeaders: ["set-cookie"],
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', authRoute);
-app.use('/api/scrape', ingestRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/scrape", ingestRoute);
 app.use(handleError);
 
-app.get('/', (req, res) => {
-    res.json({
-        "message": "hello, this is layerzero's server!"
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello, this is layerzero's server!",
+  });
+});
+
+app.get("/api/health", (req, res) => {
+  return res.status(201).json({
+    status: "OK",
+    message: "API is working properly",
+    uptime: Math.floor(process.uptime()),
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`the server is currently running on: http://localhost:${PORT}`);
-    connectWithDB();
+  console.log(`the server is currently running on: http://localhost:${PORT}`);
+  connectWithDB();
 });
--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true
+      type: String,
+      unique: true,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
-    }
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const User = mongoose.model("User", userSchema);
 
