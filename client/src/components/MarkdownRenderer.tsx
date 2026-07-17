bd07faf08972cdf175844e74140087cec13595e9
Rishabh Sharma <rishabh223300@gmail.com>
2026-07-17 14:48:30 +0530

    feat: add rehype-raw plugin to MarkdownRenderer for enhanced HTML rendering

--- client/src/components/MarkdownRenderer.tsx
+++ client/src/components/MarkdownRenderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 import { cn } from '../lib/utils';
 
 interface MarkdownRendererProps {
@@ -14,7 +15,10 @@ export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content, cla
       "prose prose-invert max-w-none prose-pre:bg-[#141414] prose-pre:border prose-pre:border-border prose-pre:p-4 prose-pre:rounded-none",
       className
     )}>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      >{content}</ReactMarkdown>
     </div>
   );
 };
