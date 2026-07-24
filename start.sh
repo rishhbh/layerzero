e172774b25d22ce7f5ddc699bb4719066a1da05c
Rishabh Sharma <rishabh223300@gmail.com>
2026-07-24 20:20:42 +0530

    feat: update startup script to clarify application name and running instructions

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # Stop script execution if any command fails
 set -e
 
-echo "Starting the application with Docker Compose..."
+echo "Starting the Layerzero with Docker Compose..."
 
 # Build the images (if changes were made) and start containers in the background
 docker compose up --build -d
 
-echo "Application is up and running!"
+echo "Layerzero is running and to stop, run 'docker compose down'."
